b00b58e5fa916d30fb894c9d60b0b8a967484c07
Tyler Hunt <tyhunt04@gmail.com>
2019-03-25 16:54:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,7 @@
-class Application 
+class Application
 
   @@items = []
-  
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -14,7 +14,7 @@ class Application
         resp.write "Item not found"
         resp.status = 400
       end
-      
+
     else
       resp.write "Route not found"
       resp.status = 404
