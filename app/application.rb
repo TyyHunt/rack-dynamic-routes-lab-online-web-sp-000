c71930b3cd724cd05da902c78bbf6a31424cd580
Tyler Hunt <tyhunt04@gmail.com>
2019-03-25 17:05:48 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,15 +8,14 @@ class Application
 
     if req.path.match(/items/)
       item_request = req.path.split("/items/").last
-      if item = @@item.find do |i|
-        i.name = item_request
-      end
+        if item = @@item.find do |i|
+          i.name = item_request
+        end
         resp.write item.price
       else
         resp.write "Item not found"
         resp.status = 400
       end
-
     else
       resp.write "Route not found"
       resp.status = 404
